8ba5d0da3da355b4207f417a062eddc165c81e74
SorkoPiko <me@sorko.dev>
2026-02-22 23:29:20 +1100

    fix: meta

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "SendDB",
+  description: "Easily track level sends.",
   navItems: [
     {
       label: "Home",
